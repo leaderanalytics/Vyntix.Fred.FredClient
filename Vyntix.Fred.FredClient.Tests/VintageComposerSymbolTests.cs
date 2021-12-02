273716d163803f7435b90b0b78f8e40b155dece9
Sam <sam.wheat@outlook.com>
2021-12-01 17:53:08 -0800

    Add test

--- Vyntix.Fred.FredClient.Tests/VintageComposerSymbolTests.cs
+++ Vyntix.Fred.FredClient.Tests/VintageComposerSymbolTests.cs
@@ -50,5 +50,33 @@ namespace LeaderAnalytics.Vyntix.Fred.FredClient.Tests
             // Make sure every date in the original list is included in the output
             var missingInVintages = vintageDates.Where(vd => !vintages.Any(v => v.Key == vd.VintageDate)).ToList();
         }
+
+        [Test]
+        public async Task DFII10_Test()
+        {
+            //
+            // FAILS - runs out of threads
+            //
+            string symbol = "DFII10";
+            DateTime endDate = new DateTime(2020, 12, 31);
+            List<Vintage> vintageDates = (await FredClient.GetVintageDates(symbol, null)).Where(x => x.VintageDate <= endDate).ToList();
+            List<Observation> sparse = (await FredClient.GetObservations(symbol, vintageDates.Select(x => x.VintageDate).ToList()))
+                .Where(x => x.ObsDate <= endDate).ToList();
+
+            Assert.AreEqual(1662, vintageDates.Count);
+            Assert.AreEqual(4503, sparse.Count);
+
+            // Group sparse observations into vintages
+            var vintages = sparse.GroupBy(x => x.VintageDate).ToList();
+            Assert.AreEqual(1662, vintages.Count);
+
+            // Make sure we did not wind up with more vintage dates than was in our original list of vintage dates.
+            var missingInVintageDates = vintages.Where(v => !vintageDates.Any(vd => vd.VintageDate == v.Key)).ToList();
+            Assert.AreEqual(0, missingInVintageDates.Count);
+
+            // Make sure every date in the original list is included in the output
+            var missingInVintages = vintageDates.Where(vd => !vintages.Any(v => v.Key == vd.VintageDate)).ToList();
+            Assert.AreEqual(0, missingInVintages.Count);
+        }
     }
 }
